f219038066843d0581df7749ac1ea7e98223e7ab
Enrique Diaz <129296040+EnriDv@users.noreply.github.com>
2025-09-05 21:19:19 -0400

    task3/more_details

--- main.kt
+++ main.kt
@@ -1,3 +1,7 @@
+enum class Genero {
+    Hombre,
+    Mujer
+}
 
 fun main() {
     println("Hola Mundo!")
@@ -5,4 +9,15 @@ fun main() {
     println("Mi nombre es: $nombre")
     val materia = "Aplicaciones Moviles"
     println("La materia es: $materia")
+    val peso : Int = 80
+    val altura : Double = 1.72
+    var edad : Int = 23
+    println("Yo peso: $peso kg")
+    println("Y mido: $altura metros")
+    println("Tengo $edad años")
+    var es_par : Boolean = (edad % 2 == 0)
+    if (es_par) { println("Mi edad es par.") }
+    else { println("Mi edad es impar.") }
+    val genero = Genero.Hombre
+    println("Yo soy $genero")
 }
